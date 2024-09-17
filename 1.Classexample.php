06c02fa12f1320762ad7485ad7a0f4d0d475df25
Adrian <adjbrz@gmail.com>
2024-09-17 17:40:20 -0400

    Se añadio un script a modo de introduccion a la poo en php

---
+++ 1.Classexample.php
@@ -0,0 +1,35 @@
+<?php
+
+/*Para crear una clase se hace uso del keyword class*/
+class Fruit {
+  /* Propiedades (pueden ser publicas, privadas y protegidas)
+    y se definen con su nivel de encapsulamiento y visibilidad mas en -->(Access modifiers.php)
+    */
+  public $name;
+  public $color;
+
+
+  // Metodos --> dictan el comportamiento que puede tomar un objeto
+  //Ejemplo para asignar y mostrar variables de los objetos
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function set_color($color) {
+    $this->color = $color;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+//Creando objeto, $nombre = new Nombre_clase();
+$apple = new Fruit();
+//Haciendo uso de metodos publicos 
+$apple->set_name('Apple');
+$apple->set_color('Red');
+echo "Name: " . $apple->get_name();
+echo "<br>";
+echo "Color: " . $apple->get_color();
+?>
